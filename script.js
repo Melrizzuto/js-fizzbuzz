fc71a0b98a6b46f5d18f7f8d132e177d4290048d
Melrizzuto <melrizzuto4@gmail.com>
2024-10-29 15:16:31 +0100

    analyzing exercise js

--- script.js
+++ script.js
@@ -7,3 +7,35 @@ console.clear()
 Scrivi un programma che stampi i numeri da 1 a 100,
 ma per i multipli di 3 stampi “Fizz” al posto del numero e per i multipli di 5 stampi Buzz.
 Per i numeri che sono sia multipli di 3 che di 5 stampi FizzBuzz. */
+
+/* obiettivo consegna
+far stampare un range di numeri da 1 a 100 ma:
+i numeri multipli di 3 con la parola Fizz
+i numeri multipli di 5 con la parola Buzz
+e i numeri che siano sia multipli di 3 e di 5 con la parola FizzBuzz
+
+dati da raccogliere:
+multipli di 3
+multipli di 5
+multipli di entrambi
+
+dati da definire:
+ - parola: Fizz
+ - Parola: Buzz
+ - Parola: FizzBuzz
+ - conteggio massimo del contatore
+*/
+
+/*
+Ragionamento:
+- se numero è % di 3 e % di 5 allora stampa FizzBuzz (applicare prima la più stringente)
+altrimenti (else if)
+- se numero è % di 3 allora stampa Fizz
+altrimenti (else if)
+- se numero è % di 5 allora stampa Buzz
+ altrimenti (else)
+- stampa 
+
+*/
+
+
